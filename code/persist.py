92db8732b7d67d85ae5ae8bb357414a526407e85
Martin <martin.ruskov@unimi.it>
2025-06-25 19:18:09 +0200

    Refactored Ngram model

--- code/persist.py
+++ code/persist.py
@@ -9,8 +9,14 @@ def find_ngram(
     n: int, lngram: str, estart: int, eend: int, etext: str
 ) -> list[tuple[str, str, str]]:
     s = Session()
-    result = s.query(Ngram).filter(Ngram.n == n, Ngram.lemmas == lngram).all()
-    return [(r.path, r.filename, r.address) for r in result]
+    result = (
+        s.query(Ngram, Verse)
+        .filter(Ngram.n == n, Ngram.lemmas == lngram)
+        .filter(Ngram.verse_id == Verse.id)
+        .all()
+    )
+    return [(r[1].path, r[1].filename, r[1].address) for r in result]
+
 
 def get_models() -> list[str]:
     s = Session()
