6ed9f793ab7630200b04d0109be0450966270864
Martin <martin.ruskov@unimi.it>
2025-11-20 18:21:53 +0100

    lemmatization for N-gram does not ignore non-alpha characters (OCS characters seem to be trated as non-alpha by python)

--- code/udpipeclient.py
+++ code/udpipeclient.py
@@ -3,7 +3,15 @@ from settings import udpipe_model
 
 url = "https://lindat.mff.cuni.cz/services/udpipe/api/process"
 
+
 def udpipe_sent_lemmatize(sent: str) -> list[tuple[str, str]]:
+    """
+    >>> udpipe_sent_lemmatize("речено б҃ъіⷭ҇ азъ г҃л҄ѭ вамъ· не противити сѧ зълоу ѣко речено бъіⷭ҇  ѣко ꙇ оц҃ь вашь нб҃с(къ) съвръшенъ естъ· ко҃н· за҃ч")
+    [('речено', 'рещи'), ('б҃ъіⷭ҇', 'бꙑти'), ('азъ', 'азъ'), ('г҃л҄ѭ', 'глаголати'), ('вамъ·', 'ваꙑ'), ('не', 'не'), ('противити', 'противити'), ('сѧ', 'себе'), ('зълоу', 'зълъ'), ('ѣко', 'ꙗко'), ('речено', 'рещи'), ('бъіⷭ҇', 'бꙑти'), ('ѣко', 'ꙗко'), ('ꙇ', 'и'), ('оц҃ь', 'отьць'), ('вашь', 'вашь'), ('нб҃с(къ)', 'небесьскъ'), ('съвръшенъ', 'съврьшенъ'), ('естъ·', 'бꙑтъи'), ('ко҃н·', 'нъ'), ('за҃ч', 'задити')]
+
+    >>> udpipe_sent_lemmatize("аще ты хощеши, можеть сїе тебѣ богъ дати")
+    [('аще', 'аще'), ('ты', 'тꙑ'), ('хощеши,', 'хощтѣти'), ('можеть', 'мощи'), ('сїе', 'сь'), ('тебѣ', 'тꙑ'), ('богъ', 'богъ'), ('дати', 'дати')]
+    """
     data = {
         "data": sent,
         "tokenizer": "",
@@ -15,11 +23,14 @@ def udpipe_sent_lemmatize(sent: str) -> list[tuple[str, str]]:
     response.raise_for_status()
 
     result = []
-    for l in response.json()["result"].split("\n"):
-        if not l or l[0] == "#":
+    for lem in response.json()["result"].split("\n"):
+        if not lem or lem[0] == "#":
             continue
-        p = l.split("\t")
-        if not p[1].isalpha():
-            continue
+        p = lem.split("\t")
+        # if not p[1].isalpha():
+            # continue
         result += [(p[1], p[2])]
     return result
+
+if __name__ == "__main__":
+    pass
